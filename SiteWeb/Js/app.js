953eb796a60430348347a72a61892bbfd5f11c1a
ASDaniello33 <157781326+ASDaniello33@users.noreply.github.com>
2024-10-23 07:40:04 +0300

    Add files via upload

---
+++ SiteWeb/Js/app.js
@@ -0,0 +1,26 @@
+gsap.from(".PresImg",{
+    x: -400,
+    duration: 0.5,
+    ease: "back",
+    delay: 1.5,
+})
+
+gsap.from(".GTitre1", {
+    x: -400,
+    ease: "back",
+    duration: 0.5,
+})
+
+gsap.from(".GTitre2", {
+    x: -400,
+    duration: 0.5,
+    ease: "back",
+    delay: 0.5,
+})
+
+gsap.from(".GTitre3", {
+    x: -400,
+    duration: 0.5,
+    ease: "back",
+    delay: 1,
+})
